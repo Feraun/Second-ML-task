329198a158229521e2db92d22712d023c75b2950
Feraun <artgrib@outlook.com>
2026-03-09 18:21:21 +0300

    Predict price by CatBoost

---
+++ app/second/predict_price_by_catboost.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from catboost import CatBoostRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import numpy as np
+
+def predict_price_by_catboost(df, configs):
+
+    x = df.drop(columns=["price"])
+
+    y = df["price"]
+
+    x = x.fillna("unknown")
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.1, random_state=42
+    )
+
+    categorical = x.select_dtypes(include=["object", "string"]).columns.tolist()
+
+    # model = CatBoostRegressor(
+    #     iterations=1000,
+    #     learning_rate=0.03,
+    #     depth=8,
+    #     verbose=False
+    # )
+    #
+    # model.fit(x_train, y_train, cat_features=categorical)
+    # pred = model.predict(x_test)
+    #
+    # rmse = np.sqrt(mean_squared_error(y_test, pred))
+    #
+    # print("RMSE:", rmse)
+
+    results = []
+
+    for cfg in configs:
+        model = CatBoostRegressor(
+            iterations=cfg["iterations"],
+            depth=cfg["depth"],
+            learning_rate=cfg["learning_rate"],
+            loss_function="RMSE",
+            verbose=False
+        )
+
+        model.fit(x_train, y_train, cat_features=categorical)
+
+        pred = model.predict(x_test)
+
+        rmse = np.sqrt(mean_squared_error(y_test, pred))
+
+        results.append({
+            "iterations": cfg["iterations"],
+            "depth": cfg["depth"],
+            "learning_rate": cfg["learning_rate"],
+            "RMSE": rmse
+        })
+
+    results_df = pd.DataFrame(results)
+
+    results_df.to_excel("model_configs.xlsx", index=False)
