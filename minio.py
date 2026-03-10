910405e2a692db241d6af25abb238f99c59d9866
Feraun <artgrib@outlook.com>
2026-03-10 21:16:45 +0300

    Начал minio

---
+++ minio.py
@@ -0,0 +1,35 @@
+import boto3
+from botocore.client import Config
+
+class S3BucketService:
+
+    def __init__(self, bucket, endpoint, access, secret):
+        self.bucket = bucket
+
+        self.client = boto3.client(
+            "s3",
+            endpoint_url=endpoint,
+            aws_access_key_id=access,
+            aws_secret_access_key=secret,
+            config=Config(signature_version="s3v4"),
+        )
+
+    def upload_file(self, local_path: str, object_name: str):
+        self.client.upload_file(
+            local_path,
+            self.bucket,
+            object_name
+        )
+
+    def list_objects(self, prefix: str):
+        response = self.client.list_objects_v2(
+            Bucket=self.bucket,
+            Prefix=prefix
+        )
+        return [obj["Key"] for obj in response.get("Contents", [])]
+
+    def delete_object(self, key: str):
+        self.client.delete_object(
+            Bucket=self.bucket,
+            Key=key
+        )
