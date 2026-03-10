1a1d0155c3c61d8d45d778fa370c4b5450513a4a
Feraun <artgrib@outlook.com>
2026-03-10 22:56:35 +0300

    Сохранение модели и датасетов при линейной регресии в minio

--- app/second/predict_price_by_linear_regression.py
+++ app/second/predict_price_by_linear_regression.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+import pickle
+
+from scipy import sparse
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.compose import make_column_transformer
@@ -7,7 +11,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
-def predict_price_by_linear_regression(df):
+from minio import S3BucketService
+
+def predict_price_by_linear_regression(df, s3service: S3BucketService):
 
     #делаем столбец числовым
     df["Суммарная мощность"] = df["Суммарная мощность"].str.replace(r"[^\d.]", "", regex=True).astype(float)
@@ -30,6 +36,8 @@ def predict_price_by_linear_regression(df):
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.1, random_state=42
     )
+
+    now = datetime.now().strftime("%Y%m%d-%H%M%S")
 
     # plt.scatter(x=X_train["Суммарная мощность"], y=y_train)
     # plt.xlabel("Суммарная мощность")
@@ -58,6 +66,49 @@ def predict_price_by_linear_regression(df):
     # трансформер получает x_train, делает fit и возвращает преобразованный x.
     # модель получает x и y_train, делает fit — обучает модель.
     lr_pipeline.fit(x_train, y_train)
+
+    #сохранение модели
+    with open(f"regression__{now}.pkl", "wb") as f:
+        pickle.dump(lr_pipeline, f)
+
+    s3service.upload_file(f"regression__{now}.pkl", f"regression__{now}.pkl")
+
+    #сохранение тренировочного и тестового датасетов
+    x_train_transformed = lr_pipeline.named_steps["features"].transform(x_train)
+    x_test_transformed = lr_pipeline.named_steps["features"].transform(x_test)
+
+    train_csr = sparse.csr_matrix(
+        x_train_transformed
+    )
+
+    test_csr = sparse.csr_matrix(
+        x_test_transformed
+    )
+
+    np.savez(
+        f"train_dataset_csr__{now}.npz",
+        data=train_csr.data,
+        indices=train_csr.indices,
+        indptr=train_csr.indptr,
+        shape=train_csr.shape
+    )
+
+    np.savez(
+        f"test_dataset_csr__{now}.npz",
+        data=test_csr.data,
+        indices=test_csr.indices,
+        indptr=test_csr.indptr,
+        shape=test_csr.shape
+    )
+
+    s3service.upload_file(f"train_dataset_csr__{now}.npz", f"train_dataset_csr__{now}.npz")
+    s3service.upload_file(f"test_dataset_csr__{now}.npz", f"test_dataset_csr__{now}.npz")
+
+    x_train.to_pickle(f"train_dataset_csr__{now}.pkl")
+    x_test.to_pickle(f"test_dataset_csr__{now}.pkl")
+
+    s3service.upload_file(f"train_dataset_csr__{now}.pkl", f"train_dataset_csr__{now}.pkl")
+    s3service.upload_file(f"test_dataset_csr__{now}.pkl", f"test_dataset_csr__{now}.pkl")
 
     # уже обученная модель предсказывает цены
     # предсказанный массив цен
